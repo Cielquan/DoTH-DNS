60c7f9946594a1e7ed19f4dd303053e829407113
Cielquan <cielquan@protonmail.com>
2020-02-16 13:21:10 +0100

    fixed setup.py for RTD

--- setup.py
+++ setup.py
@@ -23,5 +23,9 @@ import setuptools
 
 
 setuptools.setup(
-    setup_requires=["setuptools_scm"], use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    use_scm_version={
+        "write_to": "src/dothdns/version.py",
+        "write_to_template": "__version__ = {version!r}",
+    },
 )
